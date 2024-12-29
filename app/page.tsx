7818a8fba5e72548fc65571f7b6f42d17d586902
pogi7 <aaronlevitt7@gmail.com>
2024-12-29 15:53:53 -0800

    add favicon and update metadata
    Signed-off-by: pogi7 <aaronlevitt7@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,11 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
+
   const handleOnClick = (route: String) => {
     router.push(`/${route}`);
   };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-inter)]">
       <main className="flex flex-col row-start-2 items-center sm:items-start">
