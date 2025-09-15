e652b4a21fb35042acf62c00563d2e338d245be4
vliu36 <116984041+vliu36@users.noreply.github.com>
2025-09-15 11:07:24 -0700

    updated definitions

--- server/routes/devices.ts
+++ server/routes/devices.ts
@@ -5,20 +5,32 @@ const router = express.Router();
 
 /** Add a new device to the db
  *  POST /devices/new
+ *  @body name - Name of the device
+ *  @body uuid - A unique user id that serves as the device descriptor
+ *  @body powered - Is the device powered? True/False
+ *  @body poweredTimestamp - Time "powered" was last updated
+ *  @body connected - Is the device connected? True/False
+ *  @body connectedTimestamp - Time "connected" was last updated
+ *  @body color - Color of the device
+ *  @body colorTimestamp - Time "color" was last updated
+ *  @body brightness - Brightness of the device
+ *  @body brightnessTimestamp - Time "brightness" was last updated
+ *  @body user - Id of the user the device belongs to
+ *  @body lastUpdated - Last time this device was updated
 */
-router.post("/new", devicesController.addDevice)
+router.post("/new", devicesController.addDevice);
 
 /** Retrieve a device from uuid
  *  GET /devices/get/:uuid
  */
-router.get("/:uuid", devicesController.getDevice)
+router.get("/:uuid", devicesController.getDevice);
 
 /** Update existing device by uuid
  *  PUT /devices/update/:uuid
  */
-router.put("/:uuid", devicesController.updateDevice)
+router.put("/:uuid", devicesController.updateDevice);
 
 /** Delete device by uuid
  *  DELETE /devices/delete/:uuid
  */
-router.delete("/:uuid", devicesController.deleteDevice)
+router.delete("/:uuid", devicesController.deleteDevice);
